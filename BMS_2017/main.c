5da5ccedcc51f006462b1f703eb80e59d296eaaa
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-18 13:09:41 +0100

    Created Atmel Studio project

---
+++ BMS_2017/main.c
@@ -0,0 +1,18 @@
+/*
+ * BMS_2017.c
+ *
+ * Created: 18/03/17 13:02:27
+ * Author : Sondre
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
