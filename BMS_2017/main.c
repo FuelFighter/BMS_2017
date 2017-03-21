6303972229752b3327372c20c45962c2869beb0a
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-03-21 19:02:11 +0100

    Update main to test can driver

--- BMS_2017/main.c
+++ BMS_2017/main.c
@@ -5,14 +5,36 @@
  * Author : Sondre
  */ 
 
-#include <avr/io.h>
+#define F_CPU 8000000UL
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "UniversalModuleDrivers/can.h"
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
+	can_init();
+	sei();
+	
+	CanMessage_t msg;
+	msg.length = 8;
+	msg.id = 0x440;
+
+    while (1) {
+	    msg.data[0] = 0xCA;
+	    msg.data[1] = 0xFE;
+	    msg.data[2] = 0xBA;
+	    msg.data[3] = 0xBE;
+	    msg.data[4] = 0x00;
+	    msg.data[5] = 0xB0;
+	    msg.data[6] = 0x0B;
+	    msg.data[7] = 0x1E;
+
+		can_send_message(&msg);
+
+		_delay_ms(20);
     }
 }
 
