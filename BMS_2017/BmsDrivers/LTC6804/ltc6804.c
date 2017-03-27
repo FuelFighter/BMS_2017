0f8ee8a728e5dd701ebe482ade098b7b80127f14
Sondre Andersen <sondre.andersen@revolve.no>
2017-03-27 20:08:22 +0200

    Finish adapting ltc6804 driver for new spi driver.

--- BMS_2017/BmsDrivers/LTC6804/ltc6804.c
+++ BMS_2017/BmsDrivers/LTC6804/ltc6804.c
@@ -8,21 +8,19 @@
 #include "ltc6804.h"
 #include "ltc6804_pec.h"
 #include "ltc6804_definitions.h"
-#include "../UniversalModuleDrivers/spi.h"
+#include "../../UniversalModuleDrivers/spi.h"
+#include <util/delay.h>
+#include <stdint.h>
 
-
-void ltc6804_wakeUp ( void )
-{
-	uint8_t tx = DUMMY_BYTE;
-	spi_trancieve(&tx, NULL, 1);
+void ltc6804_wakeUp ( void ) {
+	spi_trancieve(NULL, NULL, 1, true);
+	_delay_us(T_WAKE_US);
 }
 
-ltc_ErrorCode_t ltc6804_sendCommand ( ltc_Command_t command )
-{
+ltc_ErrorCode_t ltc6804_sendCommand ( ltc_Command_t command ) {
 	uint32_t cmd_pec;
 	
-	switch (command)
-	{
+	switch (command) {
 	case Command_ADCV:
 		cmd_pec = (ADCV_CMD << 16) | ADCV_PEC;
 		break;
@@ -63,17 +61,15 @@ ltc_ErrorCode_t ltc6804_sendCommand ( ltc_Command_t command )
 	cmd_buffer[3] = (cmd_pec >> 0) & 0xFF;
 	
 	ltc6804_wakeUp();
-	bool transmission_successful = spi_trancieve(cmd_buffer, NULL, 4);
+	spi_trancieve(cmd_buffer, NULL, 4, true);
 	
-	return (transmission_successful ? NO_ERROR : ERROR_TIMEOUT);
+	return NO_ERROR;
 }
 	
-ltc_ErrorCode_t ltc6804_readRegisterGroup ( ltc_RegisterGroup_t registerGroup, ltc_RegisterData_t* registerData, uint16_t* com_errors )
-{
+ltc_ErrorCode_t ltc6804_readRegisterGroup ( ltc_RegisterGroup_t registerGroup, ltc_RegisterData_t* registerData, uint16_t* com_errors ) {
 	uint32_t cmd_pec;
 	
-	switch (registerGroup)
-	{
+	switch (registerGroup) {
 		case RegisterGroup_CONFIG:
 			cmd_pec = (RDCFG_CMD << 16) | RDCFG_PEC;
 			break;
@@ -104,53 +100,45 @@ ltc_ErrorCode_t ltc6804_readRegisterGroup ( ltc_RegisterGroup_t registerGroup, l
 		default: return ERROR_UNKNOWN_COMMAND;
 	}
 	
-	uint8_t tx_buffer[4];
-	tx_buffer[0] = (cmd_pec >> 24) & 0xFF;
-	tx_buffer[1] = (cmd_pec >> 16) & 0xFF;
-	tx_buffer[2] = (cmd_pec >> 8) & 0xFF;
-	tx_buffer[3] = (cmd_pec >> 0) & 0xFF;
+	uint8_t cmd_buffer[4];
+	cmd_buffer[0] = (cmd_pec >> 24) & 0xFF;
+	cmd_buffer[1] = (cmd_pec >> 16) & 0xFF;
+	cmd_buffer[2] = (cmd_pec >> 8) & 0xFF;
+	cmd_buffer[3] = (cmd_pec >> 0) & 0xFF;
 
+	// Do transmission
 	ltc6804_wakeUp();
-	bool transmission_successful = spi_trancieve(tx_buffer, NULL, COMMAND_TRANSMISSION_LENGTH);
-	
-	if ( !transmission_successful )
-	{
-		return ERROR_TIMEOUT;
-	}
-	
-	// Get rx-buffer
-	spi_trancieve(NULL, &(registerData->rawData), REGISTER_DATA_LENGTH + PEC_LENGTH);
-		
+	spi_trancieve_tx_then_rx(cmd_buffer, COMMAND_LENGTH + PEC_LENGTH, registerData->rawData, REGISTER_DATA_LENGTH + PEC_LENGTH);
+
 	// Check pec
-	uint16_t calculated_pec = pec_calculate(&(registerData->rawData), REGISTER_DATA_LENGTH);
+	uint16_t calculated_pec = pec_calculate(registerData->rawData, REGISTER_DATA_LENGTH);
 	uint16_t recieved_pec = (registerData->rawData[6] << 8) | registerData->rawData[7];
 	if (calculated_pec != recieved_pec)
 	{
 		return ERROR_PEC_MISMATCH;
 	}
 	
-	
 	return NO_ERROR;
 }
 
 ltc_ErrorCode_t ltc6804_writeConfigurationRegister ( ltc_RegisterData_t* registerData )
 {
+	// Setup command
 	uint8_t cmd_buffer[4];
 	cmd_buffer[0] = WRCFG_CMD >> 8;
 	cmd_buffer[1] = WRCFG_CMD & 0xFF;
 	cmd_buffer[2] = WRCFG_PEC >> 8;
 	cmd_buffer[3] = WRCFG_PEC & 0xFF;
-	spi_trancieve(cmd_buffer, NULL, 4);
 	
-	// Append pec
-	uint16_t pec = pec_calculate((uint8_t*)&(registerData->rawData), REGISTER_DATA_LENGTH);
+	// Append pec to register data
+	uint16_t pec = pec_calculate((uint8_t*)registerData->rawData, REGISTER_DATA_LENGTH);
 	registerData->rawData[6] = (pec >> 8) & 0xFF;
 	registerData->rawData[7] = (pec >> 0) & 0xFF;
-		
-	// Set tx-buffer
-	ltc6804_wakeUp();
-	spi_trancieve(&(registerData->rawData), NULL, REGISTER_DATA_LENGTH + PEC_LENGTH);
 	
-	bool transmission_successful = qspi_dma_do_transmission( REGISTER_TRANSMISSION_LENGTH );
-	return (transmission_successful ? NO_ERROR : ERROR_TIMEOUT);
+	// Do transmission
+	ltc6804_wakeUp();
+	spi_trancieve(cmd_buffer, NULL, COMMAND_LENGTH + PEC_LENGTH, false);
+	spi_trancieve(registerData->rawData, NULL, REGISTER_DATA_LENGTH + PEC_LENGTH, true);
+	
+	return NO_ERROR;
 }
