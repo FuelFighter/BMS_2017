6f4aa50ca8d70b34c07db4d95916a36426087038
DESKTOP-QP1DVTO\Sondre <sondreixix@gmail.com>
2017-04-06 21:06:28 +0200

    Change max temperature to 16000 to make it work without the transfer function for thermistors. Also change LOST_DATA to zero for the time being

--- BMS_2017/limits.h
+++ BMS_2017/limits.h
@@ -12,7 +12,8 @@
 #define LIMITS_CELL_VOLTAGE_MAX 42000 // Unit: 100 µV
 #define LIMITS_CELL_VOLTAGE_MIN 31500 // Unit: 100 µV
 
-#define LIMITS_TEMPERATURE_MAX 600 // Unit: 0.1 deg celcius
+//#define LIMITS_TEMPERATURE_MAX 600 // Unit: 0.1 deg celcius
+#define LIMITS_TEMPERATURE_MAX 16000 // Unit: LSB
 
 #define LIMITS_CURRENT_MAX 500 // Unit: 0.1 A
 
@@ -20,6 +21,6 @@
 #define LIMITS_PRECHARGE_TIME_MIN 3000 // ms
 #define LIMITS_PRECHARGE_RATIO_MIN 0.9
 
-#define LIMITS_MAX_LOST_BATTERY_DATA 2
+#define LIMITS_MAX_LOST_BATTERY_DATA 0
 
 #endif /* LIMITS_H_ */
